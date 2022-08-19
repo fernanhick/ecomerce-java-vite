89cbd03a095b4acb5381bd6ba8e42848d14c1b56
Fernando Hickman <fernanhick@gmail.com>
2022-08-19 17:44:05 +0100

    theme setup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { theme } from '$lib/stores'
+  </script>
+  
+  <svelte:head>
+    <meta name="color-scheme" content={$theme == 'system' ? 'light dark' :
+    $theme}/> <link rel="stylesheet" href={`/theme/${$theme}.css`} />
+  </svelte:head>
+  
+  <slot />
